ac2d35c245272605592cf0739c9ecc3243ce9998
batakasu <itsuking.tenten@gmail.com>
2026-07-04 20:04:06 +0900

    feat<views>: make schedule view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+def schedule_view(request):
+    tasks = Task.objects.all().order_by('id')
+    return render(request, 'tasks/schedule.html', {'tasks': tasks})
