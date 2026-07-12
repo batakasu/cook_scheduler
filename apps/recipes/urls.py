87979586edea8d1a966ffd3898a1f7367029475a
batakasu <itsuking.tenten@gmail.com>
2026-07-12 19:43:47 +0900

    feat(recipes): add app_name for namespace

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('', views.list_view, name='list'),
 ]
--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'tasks'
+
 urlpatterns = [
     path('schedule/', views.schedule_view, name='schedule'),
 ]
