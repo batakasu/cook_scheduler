e64e69743ab26903aea60647079d85ec20a9d7f3
batakasu <itsuking.tenten@gmail.com>
2026-07-16 19:13:56 +0900

    feat<accounts>: make signup fanction

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from . import views
+
+app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
