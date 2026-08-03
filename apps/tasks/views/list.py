59fc1107218c001ba3e719c0c4f16a894bced0ae
batakasu <itsuking.tenten@gmail.com>
2026-08-03 20:11:21 +0900

    feat<tasks>: 並び変えの選択boxのデザインを変更中

--- apps/tasks/views/list.py
+++ apps/tasks/views/list.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from apps.tasks.models import Project
 
-def list(request):# ① URLから 'sort' というパラメータを受け取る（デフォルトは 'new'）
+def list(request):
     sort_type = request.GET.get('sort', 'new')
 
-    # ② 選択された条件に合わせて order_by を切り替える
     if sort_type == 'old':
         projects = Project.objects.order_by('scheduled_at')  # 古い順
     elif sort_type == 'title':
@@ -12,7 +11,6 @@ def list(request):# ① URLから 'sort' というパラメータを受け取る
     else:
         projects = Project.objects.order_by('-scheduled_at') # 新しい順（デフォルト）
 
-    # ③ HTMLにデータと「今どれが選ばれているか」を渡す
     context = {
         'projects': projects,
         'current_sort': sort_type,
