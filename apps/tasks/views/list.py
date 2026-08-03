68c571bf5ce18873afc2d1addfccdbad8651a991
batakasu <itsuking.tenten@gmail.com>
2026-08-03 19:33:35 +0900

    feat<tasks>: 日付順に表示されるように設定'

--- apps/tasks/views/list.py
+++ apps/tasks/views/list.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from apps.tasks.models import Project
 
 def list(request):
-    projects = Project.objects.all()
+    projects = Project.objects.order_by('-scheduled_at')
     return render(request, 'tasks/list.html', {'projects': projects})
