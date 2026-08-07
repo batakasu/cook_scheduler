c1c05254941e4a1633fae997b555bfb3d4c89f61
batakasu <itsuking.tenten@gmail.com>
2026-08-07 21:17:43 +0900

    feat<tasks>: ガントチャートで表示されるようになった

--- apps/tasks/views/project_detail.py
+++ apps/tasks/views/project_detail.py
@@ -1,12 +1,33 @@
+import json
 from django.views import generic
 from apps.tasks.models import Project
+from datetime import timedelta
+
 
 class ProjectDetailView(generic.DetailView):
-    model = Project
-    template_name = 'tasks/project_detail.html'
-    context_object_name = 'project'
+  model = Project
+  template_name = 'tasks/project_detail.html'
+  context_object_name = 'project'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = self.object.tasks.all()
-        return context
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    project = self.object
+    tasks = project.tasks.all()
+    context['tasks'] = tasks
+
+    tasks_data = []
+    if project.scheduled_at:
+      for task in tasks:
+        start_time = project.scheduled_at + timedelta(minutes=task.start_offset)
+        end_time = start_time + timedelta(minutes=task.duration)
+
+        tasks_data.append({
+            'id': task.id,
+            'content': task.title,
+            'start': start_time.isoformat(),
+            'end': end_time.isoformat(),
+        })
+
+    context['tasks_json'] = json.dumps(tasks_data)
+
+    return context
