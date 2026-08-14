ca659b99e219559655f1d9070d974af58972dcd3
batakasu <itsuking.tenten@gmail.com>
2026-08-14 22:17:14 +0900

    feat<accounts>: ログインとサインアップでタブに表示される文字を変更

--- static/js/project_detail.js
+++ static/js/project_detail.js
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         },
         
-        zoomMin: 1000 * 60 * 10,    // ミリ秒 * 秒 * 分
+        zoomMin: 1000 * 60 * 30,    // ミリ秒 * 秒 * 分
         zoomMax: 1000 * 60 * 60 * 24 * 2,   // ミリ秒 * 秒 * 分 * 時 * 日
         // 
         min: startDate, // これより過去にはスクロールできない
